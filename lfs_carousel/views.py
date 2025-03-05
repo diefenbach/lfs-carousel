3d2a663d34b597695b5225b469edc57b15cfcd5b
Kai Diefenbach <kai.diefenbach@iqpp.de>
2025-03-05 13:12:13 +0100

    Change logger name to 'lfs'

--- lfs_carousel/views.py
+++ lfs_carousel/views.py
@@ -24,7 +24,7 @@ from lfs_carousel.signals import carousel_changed
 # Load logger
 import logging
 
-logger = logging.getLogger("default")
+logger = logging.getLogger("lfs")
 
 
 class LFSCarouselView(object):
